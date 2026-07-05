b36d0c695d94bedd526fe663f8a8b5da6528a69e
djolung-Git <djohari2000@gmail.com>
2026-07-05 21:23:47 +0700

    Update install script

---
+++ TailScale/autoinstall.sh
@@ -0,0 +1,27 @@
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+local_ip=$(hostname -I | awk '{print $1}')
+echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}==================== Script Install TailScale. =============================${NC}"
+echo -e "${GREEN}========================== By Djojo. =======================================${NC}"
+echo -e "${GREEN}============================================================================${NC}"
+echo -e "${GREEN}Apakah anda ingin melanjutkan? (y/n)${NC}"
+read confirmation
+if [ "$confirmation" != "y" ]; then
+    echo -e "${GREEN}Install dibatalkan. Tidak ada perubahan dalam server anda.${NC}"
+    exit 1
+fi
+for ((i = 3; i >= 1; i--)); do
+	sleep 1
+    echo "Melanjutkan dalam $i. Tekan ctrl+c untuk membatalkan"
+done
+
+## Install Tailscale
+    if ! command -v tailscale >/dev/null 2>&1; then
+    echo -e "${GREEN}================== Menginstall Tailscale ==================${NC}"
+    curl -fsSL https://tailscale.com/install.sh | sh
+    echo -e "${GREEN}================== Tailscale berhasil diinstall ==================${NC}"
+else
+    echo -e "${GREEN}================== Tailscale sudah terinstall ==================${NC}"
+fi
